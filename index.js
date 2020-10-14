e5d4f332fdb019a28f9345466a02ddc04ca227ba
taibislamdipu <mailtaibislam@gmail.com>
2020-10-14 20:49:33 +0600

    data get & post done

--- index.js
+++ index.js
@@ -15,14 +15,20 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send('Doctors portal is working!')
+    res.send('Creative Agency Server is Working!')
 })
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const orderCollection = client.db("creativeAgencyDB").collection("orders");
+    const reviewCollection = client.db("creativeAgencyDB").collection("reviews");
+    const servicesCollection = client.db("creativeAgencyDB").collection("services");
+
     console.log('creative network db connected successfully!!');
 
+
+
+    // insert order info to database
     app.post('/addOrder', (req, res) => {
         const order = req.body;
         console.log(order);
@@ -30,6 +36,35 @@ client.connect(err => {
             .then((result) => {
                 res.send(result.insertedCount > 0)
             })
+    })
+
+    // insert review info to database
+    app.post('/addReview', (req, res) => {
+        const review = req.body;
+        console.log(review);
+        reviewCollection.insertOne(review)
+            .then((result) => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    // read reviews from database
+    app.get('/reviews', (req, res) => {
+        reviewCollection.find({}).limit(3)
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    // insert new service info to database
+    app.post('/addService', (req, res) => {
+        const service = req.body;
+        console.log(service);
+        servicesCollection.insertOne(service)
+            .then((result) => {
+                res.send(result.insertedCount > 0)
+            })
+
     })
 
     /* app.post('/appointmentsByDate', (req, res) => {
